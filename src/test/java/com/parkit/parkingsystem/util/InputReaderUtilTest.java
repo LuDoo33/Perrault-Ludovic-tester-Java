ef9a87d95949f2778c1267b3917f0f531a15a450
Nicolas.le-stunff <nicolas.le-stunff@hotmail.fr>
2021-04-01 18:43:52 +0200

    ajout inputReaderUtilTest

---
+++ src/test/java/com/parkit/parkingsystem/util/InputReaderUtilTest.java
@@ -0,0 +1,24 @@
+package com.parkit.parkingsystem.util;
+
+
+import com.parkit.parkingsystem.service.InteractiveShell;
+import com.parkit.parkingsystem.util.InputReaderUtil;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.mockito.Mockito.when;
+
+
+public class InputReaderUtilTest {
+
+    private static final Scanner scan = new Scanner(System.in);
+    InputReaderUtil inputReaderUtil = new InputReaderUtil();
+
+
+
+}
+

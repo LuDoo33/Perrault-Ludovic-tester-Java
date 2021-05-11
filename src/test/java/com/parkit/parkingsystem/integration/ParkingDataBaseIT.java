4e7023de76398b40a2c942b09af63d98bc350a13
Nicolas.le-stunff <nicolas.le-stunff@hotmail.fr>
2021-05-11 13:27:47 +0200

    Modification du test

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,5 +1,6 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -50,29 +52,17 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingLotExitIt() throws Exception {
-        //testParkingACarIt();
-        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("Test");
-
+        testParkingACarIt();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processIncomingVehicle();
-        String vehiculeNumber = inputReaderUtil.readVehicleRegistrationNumber();
-
-        Ticket ticket = ticketDAO.getTicket(vehiculeNumber);
-        Date date = ticket.getInTime();
-        Calendar c = Calendar.getInstance();
-        c.setTime(date);
-        c.add(Calendar.HOUR,-1);
-        Date newDate = c.getTime();
-
-        ticket.setInTime(newDate);
-        ticketDAO.updateTicket(ticket);
-
+        Thread.sleep(1000);
         parkingService.processExitingVehicle();
+        ticketDAO.getTicket(registrationNumber);
 
+        int availableSlot = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
 
-        //TimeUnit.SECONDS.sleep(5);
-        assertNotNull(ticketDAO.getTicket(vehiculeNumber).getPrice());
-        assertNotNull(ticketDAO.getTicket(vehiculeNumber).getOutTime());
+        assertNotNull(ticketDAO.getTicket(registrationNumber).getPrice());
+        assertEquals(1, availableSlot);
+        assertNotNull(ticketDAO.getTicket(registrationNumber).getOutTime());
     }
 
 

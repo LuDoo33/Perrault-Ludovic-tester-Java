6f1be4e5e6f3035b2ecc46dd86cbe91a8f3ab695
Anatoly <anatolyrytik@gmai.com>
2022-05-26 18:08:26 +0200

    Add tests to increase test coverage

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,13 @@
 package com.parkit.parkingsystem.integration;
 
+import static org.mockito.Mockito.when;
+
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
@@ -15,60 +19,93 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
 
-    private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
-    private static ParkingSpotDAO parkingSpotDAO;
-    private static TicketDAO ticketDAO;
-    private static DataBasePrepareService dataBasePrepareService;
+  private static final DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+  private static ParkingSpotDAO parkingSpotDAO;
+  private static TicketDAO ticketDAO;
+  private static DataBasePrepareService dataBasePrepareService;
 
-    @Mock
-    private static InputReaderUtil inputReaderUtil;
+  @Mock
+  private static InputReaderUtil inputReaderUtil;
 
-    @BeforeAll
-    private static void setUp() throws Exception {
-        parkingSpotDAO = new ParkingSpotDAO();
-        parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
-        ticketDAO = new TicketDAO();
-        ticketDAO.dataBaseConfig = dataBaseTestConfig;
-        dataBasePrepareService = new DataBasePrepareService();
+  @BeforeAll
+  private static void setUp() throws Exception {
+    parkingSpotDAO = new ParkingSpotDAO();
+    parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
+    ticketDAO = new TicketDAO();
+    ticketDAO.dataBaseConfig = dataBaseTestConfig;
+    dataBasePrepareService = new DataBasePrepareService();
+  }
+
+  @AfterAll
+  private static void tearDown() {
+    dataBasePrepareService.clearDataBaseEntries();
+  }
+
+  @BeforeEach
+  private void setUpPerTest() throws Exception {
+    when(inputReaderUtil.readSelection()).thenReturn(1);
+    when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+    dataBasePrepareService.clearDataBaseEntries();
+  }
+
+  @Test
+  public void testParkingACar() {
+    ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+    parkingService.processIncomingVehicle();
+
+    Ticket testTicket = ticketDAO.getTicket("ABCDEF");
+    Assert.assertNotNull(testTicket);
+    Assert.assertFalse(testTicket.getParkingSpot().isAvailable());
+    Assert.assertEquals(1, testTicket.getParkingSpot().getId());
+    Assert.assertEquals("ABCDEF", testTicket.getVehicleRegNumber());
+
+  }
+
+  @Test
+  public void testParkingLotExit() {
+    ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+    parkingService.processIncomingVehicle();
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException ex) {
+      Thread.currentThread().interrupt();
     }
+    parkingService.processExitingVehicle();
 
-    @BeforeEach
-    private void setUpPerTest() throws Exception {
-        when(inputReaderUtil.readSelection()).thenReturn(1);
-        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
-        dataBasePrepareService.clearDataBaseEntries();
-    }
+    Ticket testTicket = ticketDAO.getTicket("ABCDEF");
+    Assert.assertNotNull(testTicket.getPrice());
+    Assert.assertNotNull(testTicket.getOutTime());
+  }
 
-    @AfterAll
-    private static void tearDown() {
-        dataBasePrepareService.clearDataBaseEntries();
-    }
+  @Test
+  public void getNextAvailableSpotCarTest() {
+    ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+    parkingService.processIncomingVehicle();
+    Assert.assertEquals(2, parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR));
+  }
 
-    @Test
-    public void testParkingACar() {
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processIncomingVehicle();
+  @Test
+  public void getNextAvailableSpotBikeTest() {
+    ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+    parkingService.processIncomingVehicle();
+    Assert.assertEquals(4, parkingSpotDAO.getNextAvailableSlot(ParkingType.BIKE));
+  }
 
-        Ticket testTicket = ticketDAO.getTicket("ABCDEF");
-        Assert.assertNotNull(testTicket);
-        Assert.assertFalse(testTicket.getParkingSpot().isAvailable());
-    }
+  @Test
+  public void updateParkingSpotCarTest() {
+    ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+    parkingService.processIncomingVehicle();
+    ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+    ParkingSpot parkingSpot2 = new ParkingSpot(2, ParkingType.CAR, false);
 
-    @Test
-    public void testParkingLotExit() {
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-
-        parkingService.processIncomingVehicle();
-        parkingService.processExitingVehicle();
-
-        Ticket testTicket = ticketDAO.getTicket("ABCDEF");
-        Assert.assertNotNull(testTicket.getPrice());
-        Assert.assertNotNull(testTicket.getOutTime());
-    }
+    Assert.assertEquals(true, parkingSpotDAO.updateParking(parkingSpot));
+    Assert.assertEquals(true, parkingSpotDAO.updateParking(parkingSpot2));
+    Assert.assertEquals(3, parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR));
+  }
 }
 

1a3f0c5f496c2c4b99012a25f16f3fe1aa49bc0f
Fercak Florian <fercak.florian@hotmail.fr>
2022-08-03 10:05:57 +0200

    commande SQL GET_TICKET et TicketDAO.getTicket() remis dans leurs etat initial

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -52,8 +52,7 @@ public class TicketDAO {
 	    ResultSet rs = ps.executeQuery();
 	    if (rs.next()) {
 		ticket = new Ticket();
-		ParkingSpot parkingSpot = new ParkingSpot(rs.getInt(1), ParkingType.valueOf(rs.getString(6)),
-			rs.getBoolean(7));
+		ParkingSpot parkingSpot = new ParkingSpot(rs.getInt(1), ParkingType.valueOf(rs.getString(6)), false);
 		ticket.setParkingSpot(parkingSpot);
 		ticket.setId(rs.getInt(2));
 		ticket.setVehicleRegNumber(vehicleRegNumber);

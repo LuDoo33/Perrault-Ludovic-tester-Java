e2b3a282fd50146bd72910f90880a07488a2c938
Fercak Florian <fercak.florian@hotmail.fr>
2022-08-04 10:15:50 +0200

    travail en cours sur le test d'Exception jettée par updateParking

--- src/test/java/com/parkit/parkingsystem/dao/ParkingSpotDAOTest.java
+++ src/test/java/com/parkit/parkingsystem/dao/ParkingSpotDAOTest.java
@@ -1,15 +1,22 @@
 package com.parkit.parkingsystem.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.Disabled;
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
+import com.parkit.parkingsystem.config.DataBaseConfig;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.model.ParkingSpot;
 
 public class ParkingSpotDAOTest {
+
+    @Mock
+    DataBaseConfig dataBaseConfig;
 
     @Test
     @DisplayName("test si une place est disponible")
@@ -34,19 +41,22 @@ public class ParkingSpotDAOTest {
 	assertThat(result).isTrue();
     }
 
-    @Disabled
     @Test
     @DisplayName("test qu'une erreur se produit quand un mauvais parametre est passé à updateParking")
-    public void testUpdateParkingOccurError() {
+    public void testUpdateParkingOccurError() throws ClassNotFoundException, SQLException {
 	// GIVEN - ARRANGE
-	ParkingSpot parkingSpot = new ParkingSpot(8, ParkingType.BIKE, false);
+
+	when(dataBaseConfig.getConnection()).thenThrow(new ClassNotFoundException(), new SQLException());
+	ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE, false);
 
 	// WHEN - ACT
+	// assertThrows(ClassNotFoundException.class, SQLException.class, () ->
+	// ParkingSpotDAO.updateParking(new ));
 	ParkingSpotDAO parkingSpotDAO = new ParkingSpotDAO();
 	boolean result = parkingSpotDAO.updateParking(parkingSpot);
 
 	// THEN - ASSERT
 	// LE RETOUR EST FALSE QUAND UNE ERREUR DE MISE A JOUR SE PRODUIT
-	// assertThat(result).isFalse();
+	assertThat(result).isFalse();
     }
 }

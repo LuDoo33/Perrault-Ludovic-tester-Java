45a8281a05c316a2313c13364e30b157009f3096
Quentin Caracatzanis <quentinversaille@gmail.com>
2022-09-05 12:43:36 +0200

    Algorithm + UnitTest + IntegrationTest
    Used LocalDateTime because we're in Java 8, facilitate reading of the code and make the algorithms work for more than a day.
    Wrote the integration Testing
    All green

--- resources/Data.sql
+++ resources/Data.sql
@@ -49,5 +49,5 @@ insert into parking(PARKING_NUMBER,AVAILABLE,TYPE) values(1,true,'CAR');
 insert into parking(PARKING_NUMBER,AVAILABLE,TYPE) values(2,true,'CAR');
 insert into parking(PARKING_NUMBER,AVAILABLE,TYPE) values(3,true,'CAR');
 insert into parking(PARKING_NUMBER,AVAILABLE,TYPE) values(4,true,'BIKE');
-insert into parking(PARKING_NUMBER,AVAILABLE,TYPE) values(5,true,'BIKE');
+insert into parking(PARKING_UMBER,AVAILABLE,TYPE) values(5,true,'BIKE');
 commit;

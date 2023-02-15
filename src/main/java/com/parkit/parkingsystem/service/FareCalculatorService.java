78bc2c5c0d7a4645b5216c00f33ac682fb7e023c
Carolina Martinez <caromarmachi@gmail.com>
2023-02-15 16:30:39 +0100

    Correction du bug dans le prix : - le methode getHours était obsolète et ne convenait pas pour le calcul. - Nous sommes passés par la méthode getTime de l'objet Date (heure de sortie et entree du ticket), pour calculer une difference en millisecondes - puis nous divisons pour obtenir une fraction d'heures pour calculer le prix

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,11 +10,21 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+//        int inHour = ticket.getInTime().getHours();
+//        int outHour = ticket.getOutTime().getHours();
+        
+        double inHour = ticket.getInTime().getTime();  // temps ecoulé en millisecondes
+        double outHour = ticket.getOutTime().getTime();// temps ecoulé  en millisecondes
+        
+        
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        double duration = outHour - inHour;  // difference en millisecondes
+        
+        duration = duration / 1000.0 / 60.0 / 60.0; // difference en fraction d 'heure
+        
+
+
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {

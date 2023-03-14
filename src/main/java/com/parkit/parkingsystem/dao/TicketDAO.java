36935b921092c1a48818c36dcfe032faedf4c36d
Carolina Martinez <caromarmachi@gmail.com>
2023-03-14 15:22:55 +0100

    Ajout de la methode getCountParkedVehicle (qui appelle la requete qui compte le nombre de vehicule garé, non sorti, pour un meme reg number)

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -107,4 +107,26 @@ public class TicketDAO {
         }
         return result;
     }
+    
+    
+    public int getCountParkedVehicle(String vehicleRegNumber){
+        Connection con = null;
+        int result=-1;
+        try {
+            con = dataBaseConfig.getConnection();
+            PreparedStatement ps = con.prepareStatement(DBConstants.COUNT_PARKED_VEHICLE);
+            ps.setString(1, vehicleRegNumber);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                result = rs.getInt(1);;
+            }
+            dataBaseConfig.closeResultSet(rs);
+            dataBaseConfig.closePreparedStatement(ps);
+        }catch (Exception ex){
+            logger.error("Error fetching next available slot",ex);
+        }finally {
+            dataBaseConfig.closeConnection(con);
+        }
+        return result;
+    }
 }

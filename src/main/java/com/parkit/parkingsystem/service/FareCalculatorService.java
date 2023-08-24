8b07ac2e7237f68d14cdb14d617e1b841df8cd5b
LSelim <ls909@free.fr>
2023-08-24 06:02:39 +0200

    Correction du bug tarif horaire pour moind d'une heure

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,27 +5,41 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
-
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
-
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
-
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
-        }
+  public void calculateFare(Ticket ticket){
+    if (ticket == null || ticket.getParkingSpot() == null) {
+        throw new IllegalArgumentException("Invalid ticket or parking spot");
     }
+
+    if (ticket.getOutTime() == null) {
+        throw new IllegalArgumentException("Out time not provided");
+    }
+
+    if (ticket.getOutTime().before(ticket.getInTime())) {
+        throw new IllegalArgumentException("Out time is before in time");
+    }
+
+    long durationInMillis = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
+double durationInHours = durationInMillis / (1000.0 * 60.0 * 60.0); // Not rounded down
+
+if (durationInHours <= 0) {
+    throw new IllegalArgumentException("Duration cannot be zero or negative");
+}
+
+// If duration is less than 1 hour but greater than zero, it will just charge based on actual duration
+
+
+    double rate;
+    switch (ticket.getParkingSpot().getParkingType()) {
+        case CAR:
+            rate = Fare.CAR_RATE_PER_HOUR;
+            break;
+        case BIKE:
+            rate = Fare.BIKE_RATE_PER_HOUR;
+            break;
+        default:
+            throw new IllegalArgumentException("Unknown Parking Type");
+    }
+
+    ticket.setPrice(durationInHours * rate);
+}
 }

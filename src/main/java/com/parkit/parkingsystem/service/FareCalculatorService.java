1a7ae4521d8c20933bc24a14fc0438bcd35704f6
LSelim <ls909@free.fr>
2023-08-26 15:00:34 +0200

    ticket02

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,43 +3,66 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+/**
+ * This class is used to calculate the fare for a given ticket.
+ */
 public class FareCalculatorService {
 
-  public void calculateFare(Ticket ticket){
-    if (ticket == null || ticket.getParkingSpot() == null) {
-        throw new IllegalArgumentException("Invalid ticket or parking spot");
+    /**
+     * Calculate the fare for a given ticket.
+     * 
+     * @param ticket
+     * @throws Exception
+     */
+    public void calculateFare(Ticket ticket) {
+        try {
+            validate(ticket);
+
+            double durationInHours = getDurationInHours(ticket);
+            double rate = getRate(ticket);
+
+            ticket.setPrice(durationInHours * rate);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    if (ticket.getOutTime() == null) {
-        throw new IllegalArgumentException("Out time not provided");
+    private void validate(Ticket ticket) throws Exception {
+        if (ticket == null || ticket.getParkingSpot() == null) {
+            throw new IllegalArgumentException("Invalid ticket or parking spot");
+        }
+
+        if (ticket.getOutTime() == null) {
+            throw new IllegalArgumentException("Out time not provided");
+        }
+
+        if (ticket.getOutTime().before(ticket.getInTime())) {
+            throw new IllegalArgumentException("Out time is before in time");
+        }
     }
 
-    if (ticket.getOutTime().before(ticket.getInTime())) {
-        throw new IllegalArgumentException("Out time is before in time");
+    private double getRate(Ticket ticket) throws Exception {
+        double rate;
+        switch (ticket.getParkingSpot().getParkingType()) {
+            case CAR:
+                rate = Fare.CAR_RATE_PER_HOUR;
+                break;
+            case BIKE:
+                rate = Fare.BIKE_RATE_PER_HOUR;
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown Parking Type");
+        }
+        return rate;
     }
 
-    long durationInMillis = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
-double durationInHours = durationInMillis / (1000.0 * 60.0 * 60.0); // Not rounded down
+    private double getDurationInHours(Ticket ticket) throws Exception {
+        long durationInMillis = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
+        double durationInHours = durationInMillis / (1000.0 * 60.0 * 60.0);
 
-if (durationInHours <= 0) {
-    throw new IllegalArgumentException("Duration cannot be zero or negative");
-}
-
-// If duration is less than 1 hour but greater than zero, it will just charge based on actual duration
-
-
-    double rate;
-    switch (ticket.getParkingSpot().getParkingType()) {
-        case CAR:
-            rate = Fare.CAR_RATE_PER_HOUR;
-            break;
-        case BIKE:
-            rate = Fare.BIKE_RATE_PER_HOUR;
-            break;
-        default:
-            throw new IllegalArgumentException("Unknown Parking Type");
+        if (durationInHours <= 0) {
+            throw new IllegalArgumentException("Duration cannot be zero or negative");
+        }
+        return durationInHours;
     }
-
-    ticket.setPrice(durationInHours * rate);
-}
 }

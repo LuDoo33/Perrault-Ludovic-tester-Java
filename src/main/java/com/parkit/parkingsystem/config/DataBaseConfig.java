473a5e78467cb64135005d3de672664d02c465f9
Sarah Schneider <sarahschneider262@gmail.com>
2023-12-01 16:04:34 +0100

    First commit / change database configuration

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -13,7 +13,7 @@ public class DataBaseConfig {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod","root","rootroot");
+                "jdbc:mysql://localhost:3406/prod","root","RootPassword90");
     }
 
     public void closeConnection(Connection con){

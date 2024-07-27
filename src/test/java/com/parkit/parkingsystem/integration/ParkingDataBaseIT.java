26e77a4c285fbe3de72b0663d8ff229a69219ebf
Thulasy BASKARAN <thulasy95@gmail.com>
2024-07-27 23:01:02 +0200

    Test Intégration de ParkingService avec la base de données : Incoming / Exiting / ExitingRecuringUser

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -4,6 +4,8 @@ import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,7 +15,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -22,6 +26,7 @@ public class ParkingDataBaseIT {
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
+    private static ParkingService parkingService;
 
     @Mock
     private static InputReaderUtil inputReaderUtil;
@@ -33,13 +38,15 @@ public class ParkingDataBaseIT {
         ticketDAO = new TicketDAO();
         ticketDAO.dataBaseConfig = dataBaseTestConfig;
         dataBasePrepareService = new DataBasePrepareService();
+
     }
 
     @BeforeEach
     private void setUpPerTest() throws Exception {
-        when(inputReaderUtil.readSelection()).thenReturn(1);
-        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+    	when(inputReaderUtil.readSelection()).thenReturn(1); //Return ParkingType.CAR
+    	when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
         dataBasePrepareService.clearDataBaseEntries();
+           
     }
 
     @AfterAll
@@ -49,17 +56,42 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingACar(){
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+    	//GIVEN
+    	parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+    	
+    	// WHEN
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        
+        // THEN
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        //Check that a ticket is actually saved in DB :
+        assertNotNull(ticket);
+        assertEquals("ABCDEF", ticket.getVehicleRegNumber());
+        //and Parking table is updated with availability :
+        ParkingSpot parkingSpot = ticket.getParkingSpot();
+        assertNotNull(parkingSpot);
+        assertFalse(parkingSpot.isAvailable());
     }
 
     @Test
     public void testParkingLotExit(){
-        testParkingACar();
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
+      	testParkingACar(); //Entry
+      	parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processExitingVehicle();//Exit
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        //Check that the fare generated and out time are populated correctly in the database
+        assertNotNull(ticket.getOutTime());
+        assertTrue(ticket.getPrice()==0); //Because stayed in the parking less than 30 minutes so free
+    }
+    
+    @Test
+    public void testParkingLotExitRecurringUser(){
+    	testParkingLotExit();//Was once in the parking
+    	testParkingACar(); //Enter the second time
+        parkingService.processExitingVehicle();//exit the second time
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        //Check the ticket price for a recurring user :
+        assertTrue(ticket.getPrice()==0); //Because stayed in the parking less than 30 minutes so free       
     }
 
 }
